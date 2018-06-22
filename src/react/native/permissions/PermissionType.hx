a6f90f0a14c276b8010229b5a916d93b730a05e0
Kevin Leung <kevinresol@gmail.com>
2018-06-22 17:53:06 +0800

    Update permission types

--- src/react/native/permissions/PermissionType.hx
+++ src/react/native/permissions/PermissionType.hx
@@ -5,13 +5,18 @@ abstract PermissionType(String) {
 	var Location = 'location';
 	var Camera = 'camera';
 	var Microphone = 'microphone';
-	var Storage = 'photo';
+	var Photo = 'photo';
 	var Contacts = 'contacts';
 	var Event = 'event';
-	#if ios
-	var Reminder = 'reminder';
 	var Bluetooth = 'bluetooth';
+	var Reminder = 'reminder';
 	var Notification = 'notification';
 	var BackgroundRefresh = 'backgroundRefresh';
-	#end 
+	var SpeechRecognition = 'speechRecognition';
+	var MediaLibrary = 'mediaLibrary';
+	var Motion = 'motion';
+	var Storage = 'storage';
+	var CallPhone = 'callPhone';
+	var ReadSms = 'readSms';
+	var ReceiveSms = 'receiveSms';
 }
